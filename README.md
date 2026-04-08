7a09696ad6576e8bf5d5b5c4fb32f12e4c0fe898
Prince Addo <117764655+Princeaddo1199@users.noreply.github.com>
2026-04-08 07:36:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GCTU-Student-Internship-System
+A student internship system to help track students doing their internship, taking records of daily activities , tasks, taking attendance, etc
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>GCTU STUDENT INTERNSHIP SYSTEM</h1>
+    </>
+  )
+}
+
+export default App
