4a46ca2a04c34df28b5d41f35c6c06ba5bbfc961
Prince Addo <117764655+Princeaddo1199@users.noreply.github.com>
2026-04-19 09:28:57 +0000

    Students Table created.
    Created the Students table and added some dummy data to check it performance and efficciency

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import BigCard from "../Components/BigCard";
 import { Building2, CalendarCheck, TriangleAlert, Users } from "lucide-react";
+import students from "../Data/studentTable.json";
+import { DataGrid } from "@mui/x-data-grid";
+import "../STYLES/Dashboard.css";
+
+const columns = [
+  { field: "student", headerName: "Student", width: 250 },
+  { field: "company", headerName: "Company", width: 200 },
+  { field: "progress", headerName: "Progress", width: 110 },
+  { field: "lastLog", headerName: "Last Log", width: 160 },
+  { field: "risk", headerName: "risk", width: 200 },
+];
 export default function Dashboard() {
   return (
     <div>
       <div className="dashboardTitle">
         <h2 className="dashboardTitleMain">Academic Supervisor Dashboard</h2>
-        <p className="dashboardTitleSub">Monitor student internships progress and performance</p>
+        <p className="dashboardTitleSub">
+          Monitor student internships progress and performance
+        </p>
       </div>
       <div className="dashboard-cards">
         <BigCard
@@ -42,6 +55,25 @@ export default function Dashboard() {
           }}
           AlertNotification="Late submissions"
         />
+      </div>
+      <div className="tableBarContainer">
+        <div className="studentsTableContainer">
+          <p style={{fontWeight: "bold", fontSize: "20px", marginBottom: "10px"}}>Student Monitoring</p>
+          <DataGrid
+            rows={students}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5]}
+            pagination
+          />
+        </div>
+        <div className="progressTrendContainer">
+          <p style={{fontWeight: "bold", fontSize: "20px", marginBottom: "10px"}}>Avg. Progress Bar</p>
+        </div>
       </div>
     </div>
   );
