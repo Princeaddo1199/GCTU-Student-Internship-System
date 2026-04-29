0c744b224c6bb34496d4650f5a5cb07b1fc0db8f
Prince Addo <117764655+Princeaddo1199@users.noreply.github.com>
2026-04-29 10:09:29 +0000

    Performance evaluation and rating
    Working on the performance Evaluation page. Working on the Evaluate student page. Yet to add the rating

--- src/Pages/PerformanceEvaluation.jsx
+++ src/Pages/PerformanceEvaluation.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import { useState } from 'react';
+import "../STYLES/PerformanceEvaluation.css"
+import { CalendarCheck } from 'lucide-react';
+import Students from "../Data/studentTable.json"
 
 export default function PerformanceEvaluation() {
+  const [activeView, setActiveView] = useState("evaluation");
+  const [selectedId, setSelectedId] = useState("")
   return (
-    <div>Performance Evaluation</div>
+    <>
+    <div className="performanceEvaluationTitle">
+        <h2 className="performanceEvaluationTitleMain">Performance Evaluation</h2>
+        <p className="performanceEvaluationTitleSub">
+          Evaluate student performanve and view scorecards
+        </p>
+      </div>
+
+      <div className="performanceEvaluationTabs">
+        <button
+          type="button"
+          className={`performanceEvaluationTabButton ${activeView === "evaluation" ? "active" : ""}`}
+          onClick={() => setActiveView("evaluation")}
+        >
+          Evaluate Student
+        </button>
+        <button
+          type="button"
+          className={`performanceEvaluationTabButton ${activeView === "reports" ? "active" : ""}`}
+          onClick={() => setActiveView("reports")}
+        >
+          Performance Scorecard
+        </button>
+      </div>
+
+      {activeView === "evaluation" ? (
+      <div className="evaluationPageMain">
+        <div className="containerHead">
+          <CalendarCheck size={16}/>
+          <p style={{marginLeft: "10px", fontSize: "16px", fontWeight: "bold"}}>New Evaluation</p>
+        </div>
+        <div className="selectStudentOption">
+        <select
+                className="selectContainer"
+                value={selectedId}
+                onChange={(e) => setSelectedId(e.target.value)}
+              >
+                <option value="TODAY">Today</option>
+                {Students.map((student) => (
+                  <option key={student.id} value={student}>
+                    {student.student}
+                  </option>
+                ))}
+              </select>
+        </div>
+      </div>
+    ) : (
+      <div className="scoreboardPageMain">
+        hh
+      </div>
+    )}
+    </>
   )
 }
