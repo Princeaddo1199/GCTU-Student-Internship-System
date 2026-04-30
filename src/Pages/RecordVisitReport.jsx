f5dc7436bdcf09971ffe486ac6bad9e47a6e71c1
Prince Addo <117764655+Princeaddo1199@users.noreply.github.com>
2026-04-30 21:28:40 +0000

    Updated the Record Visitor Report
    Working on the record visitor report

--- src/Pages/RecordVisitReport.jsx
+++ src/Pages/RecordVisitReport.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function RecordVisitReport() {
   return (
-    <div>RecordVisitReport</div>
+    <>
+          <div className="industryReportsTitle">
+            <h2 className="industryReportsTitleMain">
+              Visit Reports
+            </h2>
+            <p className="industryReportsTitleSub">
+              Record internship visit documentation
+            </p>
+          </div>
+          <div className="inputDataContainer">
+            
+          </div>
+          
+    
+          
+        </>
   )
 }
