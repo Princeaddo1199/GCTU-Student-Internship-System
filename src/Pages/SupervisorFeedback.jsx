fa90d4d5653174fc24fc5f322b4fc83ed9d8329b
Prince Addo <117764655+Princeaddo1199@users.noreply.github.com>
2026-04-30 07:58:22 +0000

    Update SupervisorFeedback.jsx
    Started working on the Supervisor feedback page

--- src/Pages/SupervisorFeedback.jsx
+++ src/Pages/SupervisorFeedback.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../STYLES/PerformanceEvaluation.css"
 
 export default function SupervisorFeedback() {
+  const [activeView, setActiveView] = useState("evaluation");
+
   return (
-    <div>Supervisor Feedback</div>
+    <>
+        <div className="performanceEvaluationTitle">
+            <h2 className="performanceEvaluationTitleMain">Performance Evaluation</h2>
+            <p className="performanceEvaluationTitleSub">
+              Evaluate student performanve and view scorecards
+            </p>
+          </div>
+           <div className="performanceEvaluationTabs">
+            <button
+              type="button"
+              className={`performanceEvaluationTabButton ${activeView === "evaluation" ? "active" : ""}`}
+              onClick={() => setActiveView("evaluation")}
+            >
+              Evaluate Student
+            </button>
+            <button
+              type="button"
+              className={`performanceEvaluationTabButton ${activeView === "reports" ? "active" : ""}`}
+              onClick={() => setActiveView("reports")}
+            >
+              Performance Scorecard
+            </button>
+          </div>
+    
+          {activeView === "evaluation" ? (
+          <div className="evaluationPageMain">
+            <div>hiii</div>
+          </div>
+        ) : (
+          <div className="scoreboardPageMain">
+        <div>heloo</div>
+      </div>
+        )}
+        </>
   )
 }
